3adbaaa172cb66a9f7d1a9d0c0333bb68bb6a5b7
unknown <ayushbhaimehta20002@gmail.com>
2022-06-19 03:44:22 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,13 @@
+const animals = [
+  {
+    name: "cat",
+    sound: "meow",
+    feedinRequirements: {
+      food: 2,
+      water: 3
+    }
+  },
+  { name: "dog", sound: "woof" }
+];
+
+export default animals;
---
+++ src/index.js
@@ -0,0 +1,49 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+// import animals from "./data";
+import cars from "./practice";
+
+// console.log(animals);
+// const [cat, dog] = animals;
+// console.log(cat);
+// const { name: catName, sound: catSound } = cat;
+// console.log(catName);
+
+console.log(cars);
+const [honda, tesla] = cars;
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+
+const {
+  coloursByPopularity: { hondaTopColour }
+} = honda;
+const {
+  coloursByPopularity: { teslaTopColour }
+} = honda;
+
+console.log(honda);
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
